cdf1ca2fd735097eb4a83786721b1fe8c69498bd
prince1096 <princeraj10121996@gmail.com>
2022-11-26 19:08:56 +0530

    areaDone

--- area.js
+++ area.js
@@ -2,30 +2,29 @@ const firstSideInput = document.getElementById("first-side");
 const secondSideInput = document.getElementById("second-side");
 const thirdSideInput = document.getElementById("third-side");
 const button = document.querySelector(".button");
-const outputEl = document.querySelector(".output");
+const outputEl = document.querySelector("#output");
 
 button.addEventListener("click", calculateArea);
 
-const firstSide = Number(firstSideInput.value);
-const secondSide = Number(secondSideInput.value);
-const thirdSide = Number(thirdSideInput.value);
-
 function calculateArea(e) {
   e.preventDefault();
+
+  const firstSide = Number(firstSideInput.value);
+  const secondSide = Number(secondSideInput.value);
+  const thirdSide = Number(thirdSideInput.value);
+
   if (
     firstSide + secondSide > thirdSide &&
     secondSide + thirdSide > firstSide &&
     firstSide + thirdSide > secondSide
   ) {
-    const finalSum = semiPeri(
-      Number(firstSide.value),
-      Number(secondSide.value),
-      Number(thirdSide.value)
-    );
+    const finalSum = semiPeri(firstSide, secondSide, thirdSide);
     const area = Math.sqrt(finalSum);
     outputEl.innerText = "The Area of Triangle is " + area;
+    // console.log(1);
   } else {
     outputEl.innerText = "Enter Valid sides";
+    // console.log(2);
   }
 }
 
